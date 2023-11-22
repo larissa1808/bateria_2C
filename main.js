3a572f7e5f72d26832ab6fceadc837ca7681d642
larissa1808 <129516821+larissa1808@users.noreply.github.com>
2023-11-22 11:51:29 -0300

    .

--- main.js
+++ main.js
@@ -28,17 +28,17 @@ function tocaSomToim (){
     }
     document.querySelector('.tecla_toim') .onclick = tocaSomToim;
 
-    function tocaSomPsh (){
-        document.querySelector('#som_tecla_psh').play();
-        }
-        document.querySelector('.tecla_psh') .onclick = tocaSomPsh;
+function tocaSomPsh (){
+    document.querySelector('#som_tecla_psh').play();
+    }
+    document.querySelector('.tecla_psh') .onclick = tocaSomPsh;
 
-        function tocaSomTic (){
-            document.querySelector('#som_tecla_tic').play();
-            }
-            document.querySelector('.tecla_tic') .onclick = tocaSomTic;
+function tocaSomTic (){
+    document.querySelector('#som_tecla_tic').play();
+     }
+    document.querySelector('.tecla_tic') .onclick = tocaSomTic;
 
-            function tocaSomTom (){
-                document.querySelector('#som_tecla_Tom').play();
-                }
-                document.querySelector('.tecla_tom') .onclick = tocaSomTom;
+function tocaSomTom (){
+    document.querySelector('#som_tecla_Tom').play();
+     }
+    document.querySelector('.tecla_tom') .onclick = tocaSomTom;
