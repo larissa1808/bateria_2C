e9c08409cb02de7f26b6c1f55f98feabaa1a4e1c
larissa1808 <129516821+larissa1808@users.noreply.github.com>
2023-11-22 11:49:45 -0300

    .

--- main.js
+++ main.js
@@ -27,3 +27,18 @@ function tocaSomToim (){
     document.querySelector('#som_tecla_toim').play();
     }
     document.querySelector('.tecla_toim') .onclick = tocaSomToim;
+
+    function tocaSomPsh (){
+        document.querySelector('#som_tecla_psh').play();
+        }
+        document.querySelector('.tecla_psh') .onclick = tocaSomPsh;
+
+        function tocaSomTic (){
+            document.querySelector('#som_tecla_tic').play();
+            }
+            document.querySelector('.tecla_tic') .onclick = tocaSomTic;
+
+            function tocaSomTom (){
+                document.querySelector('#som_tecla_Tom').play();
+                }
+                document.querySelector('.tecla_tom') .onclick = tocaSomTom;
